fd658e6bef84c213d06d5c3e3556fd1e05ac5dbc
rvkrvyoj <rlafbfgml124@naver.com>
2024-12-10 18:04:33 +0900

    2024년 12월 10일 복습

---
+++ 2024_12_10 로그인 페이지 만들기주차 강좌 복습.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2024_12_10</title>
+    <style>
+        .mytitle {
+            color : red
+        }
+    </style>
+</head>
+<body>
+        <h1 class "mytitle">로그인 페이지</h1>
+        <p>ID : <input type = "text" /></p>
+        <p>Password : <input type = "text" /></p>
+        <button>확인</button>
+</body>
+</html>
